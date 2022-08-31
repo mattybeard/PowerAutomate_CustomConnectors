3d4b72af4408fb66fcd20f07cae05d695e14610e
Alexander Worley <jAlexanderworley@gmail.com>
2022-08-31 14:30:48 -0400

    Regex Replace AND Match in One
    I updated and combined both Replace and Match into a single c# file and swagger. I wish to append this to this great piece of work as the other options in Power Automate are very sparse and often behind paywalls. I tested it and confirmed everything works as expected.

---
+++ Alexanders RegEx (independent Publisher)/RegEx_Code.cs
@@ -0,0 +1,67 @@
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using System.Net;
+
+public class Script : ScriptBase
+{
+    public override async Task<HttpResponseMessage> ExecuteAsync()
+    {
+        if (this.Context.OperationId == "RegexReplace")
+        {
+            return await this.PerformRegexReplace().ConfigureAwait(false);
+        }else if(this.Context.OperationId == "RegexMatch") {
+            return await this.PerformRegexMatch().ConfigureAwait(false);
+        }
+
+        HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.BadRequest);
+        response.Content = CreateJsonContent($"Unknown operation ID '{this.Context.OperationId}'");
+        return response;
+    }
+private async Task<HttpResponseMessage> PerformRegexReplace()
+    { 
+        // Manipulate the request data as applicable before setting it back
+        var requestContentAsString = await this.Context.Request.Content.ReadAsStringAsync().ConfigureAwait(false);
+        var requestContentAsJson = JObject.Parse(requestContentAsString);
+
+        var input = (string)requestContentAsJson["input"];
+        var pattern = (string)requestContentAsJson["pattern"];
+        var replacement = (string)requestContentAsJson["replacement"];
+
+        var regexResult = Regex.Replace(input, pattern, replacement);
+
+        // Manipulate the response data as applicable before returning it
+        JObject output = new JObject
+        {
+            ["output"] = regexResult
+        };
+
+        var response = new HttpResponseMessage(HttpStatusCode.OK);
+        response.Content = CreateJsonContent(output.ToString());
+        return response;
+    }
+    private async Task<HttpResponseMessage> PerformRegexMatch()
+    { 
+        // Manipulate the request data as applicable before setting it back
+        var requestContentAsString = await this.Context.Request.Content.ReadAsStringAsync().ConfigureAwait(false);
+        var requestContentAsJson = JObject.Parse(requestContentAsString);
+
+        var input = (string)requestContentAsJson["input"];
+        var pattern = (string)requestContentAsJson["pattern"];
+        
+
+        var regexResult = Regex.Match(input, pattern).Value;
+
+        // Manipulate the response data as applicable before returning it
+        JObject output = new JObject
+        {
+            ["output"] = regexResult
+        };
+
+        var response = new HttpResponseMessage(HttpStatusCode.OK);
+        response.Content = CreateJsonContent(output.ToString());
+        return response;
+    }
+    
+}
